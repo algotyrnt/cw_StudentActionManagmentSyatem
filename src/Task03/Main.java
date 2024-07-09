7948d8599414d4c94bb8dbf4a1ec43d2f5664245
Punjitha Bandara <punjitha@gmail.com>
2024-07-09 21:14:23 +0530

    task 03 completed, added comments for task 03

--- src/Task03/Main.java
+++ src/Task03/Main.java
@@ -1,3 +1,11 @@
+/**
+ * Project Name: cw_StudentActionManagementSystem
+ * File Name: Main.java
+ * Description: This has the main functionality of the program.
+ * Author: Punjitha Bandara - w2083155
+ * Start Date: June 23, 2024
+ */
+
 package Task03;
 
 import java.io.File;
@@ -7,13 +15,21 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
-    private static int studentCount = 0;
-    private static final Student[] students = new Student[100];
+    private static int studentCount = 0; // Holds the count of registered students
+    private static final Student[] students = new Student[100]; // Array to store student objects, with a maximum of 100 students
 
+    /**
+     * The main method that initiates the program.
+     */
     public static void main(String[] args) {
         menu();
     }
 
+    /**
+     * Displays the main menu and handles user input for various functionalities.
+     * Continuously displays the menu until a valid choice is made.
+     * Handles invalid inputs gracefully by prompting the user to try again.
+     */
     private static void menu(){
         Scanner scanner = new Scanner(System.in);
         int choice;
@@ -32,11 +48,11 @@ public class Main {
             System.out.println("Enter your choice");
             try {
                 choice = scanner.nextInt();
-                scanner.nextLine();
             } catch (InputMismatchException e) {
                 System.err.println("Invalid input, please try again");
-                System.out.println();
                 continue;
+            }finally {
+               scanner.nextLine(); // Consume the newline left-over
             }
 
             switch (choice) {
@@ -80,54 +96,71 @@ public class Main {
         }
     }
 
+    /**
+     * Checks and displays the number of available seats.
+     * Calculates available seats by subtracting the current student count from the total capacity.
+     */
     private static void checkAvailableSeats(){
         System.out.println("\nSeat Availability\n");
         System.out.println("Available seats: " + (students.length - studentCount));
     }
 
+    /**
+     * Registers a new student if there is an available seat.
+     * Prompts the user for a student ID and name, checks for ID uniqueness, and adds the student to the array.
+     * @param scanner Scanner object for reading user input.
+     */
     private static void registerStudent(Scanner scanner) {
         System.out.println("\nStudent Registration\n");
         if (100 > studentCount) {
-            while (true) {
-                System.out.println("Enter student ID: ");
-                String studentID = scanner.next();
-                scanner.nextLine();
-                if (!(studentID.startsWith("w") && studentID.length() == 8)) {
-                    System.err.println("Student ID must start with 'w' and contains 8 characters");
-                    continue;
+            String studentID = studentIDInput(scanner);
+            for (int i = 0; i < studentCount; i++) {
+                if (students[i].getID().equals(studentID)) {
+                    System.err.println("Student ID already exists.");
+                    return;
                 }
-                for (Student student : students) {
-                    if(student != null && studentID.equals(student.getID())){
-                        System.err.println("Student ID already exists.");
-                        return;
-
-                    }
-                }
-                for (int i = 0; i < students.length; i++) {
-                    if (students[i] == null) {
-                        System.out.println("Enter student name: ");
-                        String studentName = scanner.nextLine();
-                        students[i] = new Student(studentID, studentName);
-                        studentCount++;
-                        break;
-                    }
-                }
-                break;
             }
+            System.out.println("Enter student name: ");
+            String studentName = scanner.nextLine();
+            students[studentCount] = new Student(studentID, studentName);
+            studentCount++;
         } else {
             System.out.println("There are no more seats available");
         }
     }
 
-    private static void deleteStudent(Scanner scanner){
-        System.out.println("\nDelete a Registered Student\n");
-        if(studentCount > 0){
+    /**
+     * Prompts the user for a student ID, ensuring it starts with 'w' and contains 8 characters.
+     * @param scanner Scanner object for reading user input.
+     * @return The validated student ID.
+     */
+    private static String studentIDInput(Scanner scanner){
+        while (true) {
             System.out.println("Enter student ID: ");
-            String studentID = scanner.next();
-            for (int i = 0; i < students.length; i++) {
-                if (students[i] != null && studentID.equals(students[i].getID())) {
-                    students[i] = null;
+            String studentID = scanner.nextLine();
+            if (!(studentID.startsWith("w") && studentID.length() == 8)) {
+                System.err.println("Invalid ID, student ID must start with 'w' and contains 8 characters");
+            }else {
+                return studentID;
+            }
+        }
+    }
+
+    /**
+     * Deletes a student from the system based on the provided student ID.
+     * @param scanner Scanner object for reading user input.
+     */
+    private static void deleteStudent(Scanner scanner){
+        if(studentCount > 0){
+            System.out.println("\nDelete a Registered Student\n");
+            String studentID = studentIDInput(scanner);
+            for (int i = 0; i < studentCount; i++) {
+                if (studentID.equals(students[i].getID())) {
                     studentCount--;
+                    for (int j = 0; j < studentCount; j++) {
+                        students[j] = students[j + 1];
+                    }
+                    students[studentCount] = null;
                     System.out.println("Student ID: " + studentID + " deleted.");
                     break;
                 }
@@ -137,46 +170,51 @@ public class Main {
         }
     }
 
+    /**
+     * Finds and displays a student's details based on the provided student ID.
+     * @param scanner Scanner object for reading user input.
+     */
     private static void findStudent(Scanner scanner){
         if (studentCount > 0){
             System.out.println("\nFind a Registered Student\n");
-            System.out.println("Enter student ID: ");
-            String studentID = scanner.next();
-            for (Student student : students) {
-                if (student != null && studentID.equals(student.getID())) {
-                    System.out.println("Student ID: " + studentID);
-                    System.out.println("Student Name: " + student.getName());
+            String studentID = studentIDInput(scanner);
+            for (int i = 0; i < studentCount; i++) {
+                if (studentID.equals(students[i].getID())) {
+                    System.out.println("Student ID: " + studentID + " Student Name: " + students[i].getName());
                     return;
                 }
             }
             System.err.println("Student ID not found.");
-
         }else {
-            System.err.println("There is no student details available to find with student ID.");
+            System.err.println("\nThere is no student details available to find with student ID.");
         }
     }
 
+    /**
+     * Saves the details of all registered students to a file named "StudentDetails.txt".
+     */
     private static void saveToFile(){
         if (studentCount > 0){
             System.out.println("\nSave Student Details to StudentDetails.txt File\n");
             try {
                 FileWriter file = new FileWriter("StudentDetails.txt");
-                for (Student studentDetail : students) {
-                    if (studentDetail != null) {
-                        file.write(studentDetail.getID() + "," + studentDetail.getName() + studentDetail.studentModuleMark() + "\n");
-                    }
+                for (int i = 0; i < studentCount; i++) {
+                    file.write(students[i].getID() + "," + students[i].getName() + students[i].studentModuleMark() + "\n");
                 }
                 System.out.println("Student details saved.");
                 file.close();
             } catch (IOException e) {
                 System.err.println("Error while writing to the file");
-                e.printStackTrace();
+                //e.printStackTrace();
             }
         }else{
             System.err.println("There is no student details available to save to StudentDetails.txt file.");
         }
     }
 
+    /**
+     * Loads student details from a file named "StudentDetails.txt" and adds them to the system.
+     */
     private static void loadFromFile(){
         System.out.println("\nLoad Student Details from StudentDetails.txt File\n");
         try{
@@ -201,21 +239,18 @@ public class Main {
             fileReader.close();
         }catch(IOException e){
             System.err.println("Error while reading from file");
-            e.printStackTrace();
+            //e.printStackTrace();
         }
     }
 
+    /**
+     * Sorts and displays the list of students based on their names in alphabetical order.
+     */
     private static void sortStudents(){
-        System.out.println("\nView the list of students sorted in alphabetical order\n");
         if (studentCount > 0) {
+            System.out.println("\nView the list of students sorted in alphabetical order\n");
             Student[] namesSorted = new Student[studentCount];
-            int x = 0;
-            for (Student student : students) {
-                if (student != null) {
-                    namesSorted[x] = student;
-                    x++;
-                }
-            }
+            System.arraycopy(students, 0, namesSorted, 0, studentCount);
             for (int i = 0; i < namesSorted.length ; i++) {
                 for (int j = i + 1; j < namesSorted.length; j++) {
                     if (namesSorted[i].getName().compareToIgnoreCase(namesSorted[j].getName()) > 0) {
@@ -233,6 +268,12 @@ public class Main {
         }
     }
 
+    /**
+     * Displays more options for the user to perform additional actions.
+     * Continuously displays the options until a valid choice is made.
+     * Handles invalid inputs gracefully by prompting the user to try again.
+     * @param scanner Scanner object for reading user input.
+     */
     private static void moreOptions(Scanner scanner){
         String choice;
 
@@ -273,52 +314,65 @@ public class Main {
         }
     }
 
+    /**
+     * Adds or updates the name of a student based on the provided student ID.
+     * Prompts the user for a student ID and new name, then updates the student's name if the ID is found.
+     * @param scanner Scanner object for reading user input.
+     */
     private static void addName(Scanner scanner){
         if (studentCount > 0){
             System.out.println("\nAdd/Update Student Name\n");
-            System.out.println("Enter student ID: ");
-            String studentID = scanner.nextLine();
-            for (Student student : students) {
-                if (student != null && studentID.equals(student.getID())) {
+            String studentID = studentIDInput(scanner);
+            for (int i = 0; i < studentCount; i++) {
+                if (studentID.equals(students[i].getID())) {
                     System.out.println("Enter student name: ");
-                    String studentName = scanner.nextLine();
-                    student.setName(studentName);
+                    students[i].setName(scanner.nextLine());
                     System.out.println("New student name updated.");
                     return;
                 }
             }
             System.err.println("Student ID not found.");
-
         }else {
             System.err.println("There is no student details available add/update name with student ID.");
         }
     }
 
+    /**
+     * Adds module marks for a specific student identified by their student ID.
+     * Prompts the user for a student ID and module marks, then updates the student's module marks if the ID is found.
+     * Ensures that the entered marks are within the valid range (0 to 100).
+     * @param scanner Scanner object for reading user input.
+     */
     private static void addModuleMarks(Scanner scanner){
         if (studentCount > 0){
             System.out.println("\nAdd Module Marks\n");
-            System.out.println("Enter student ID: ");
-            String studentID = scanner.nextLine();
-            for (Student student : students) {
-                if (student != null && studentID.equals(student.getID())) {
-                    System.out.println("Student ID: " + studentID + "  Student Name: " + student.getName());
+            String studentID = studentIDInput(scanner);
+            for (int i = 0; i < studentCount; i++) {
+                if (studentID.equals(students[i].getID())) {
+                    System.out.println("Student ID: " + studentID + "  Student Name: " + students[i].getName());
                     Module[] modules = new Module[3];
-                    for (int i = 0; i < 3; i++) {
-                        System.out.print("Enter module " + (i + 1) + " mark: ");
+                    for (int j = 0; j < 3; j++) {
                         double mark;
                         while (true){
+                            System.out.print("Enter module " + (j + 1) + " mark: ");
                             try {
                                 mark = scanner.nextDouble();
-                                scanner.nextLine();
                             } catch (InputMismatchException e) {
                                 System.err.println("Invalid input, please enter a valid mark");
+                                continue;
+                            }finally {
+                                scanner.nextLine();
+                            }
+                            if (mark > 100 || mark < 0) {
+                                System.err.println("Invalid mark range, please enter a valid mark");
                                 continue;
                             }
                             break;
                         }
-                        modules[i] = new Module(mark);
-                        student.setModule(modules);
+                        modules[j] = new Module(mark);
                     }
+                    students[i].setModule(modules);
+                    System.out.println("Module marks added.");
                     return;
                 }
             }
@@ -328,20 +382,53 @@ public class Main {
         }
     }
 
+    /**
+     * Generates a summary of the system, displaying the total number of students and the number of students who have passed all modules.
+     * A student is considered to have passed if they score more than 40 marks in all modules.
+     */
     private static void generateSummary(){
-        System.out.println("\nSummary of the System\n");
-        System.out.println("* The total student registrations - " + studentCount);
+        if (studentCount > 0) {
+            System.out.println("\nSummary of the System\n");
+            System.out.println("* The total student registrations - " + studentCount);
 
-        int passStudents = 0;
-        for (Student student : students) {
-            if (student != null) {
-                passStudents += student.countModulePassStudent();
+            int passStudents = 0;
+            for (int i = 0; i < studentCount; i++) {
+                passStudents += students[i].countModulePassStudent();
             }
+            System.out.println("* Total no of student scored more than 40 marks in all modules - " + passStudents);
+        }else{
+            System.err.println("There is no student details available to generate a summary.");
         }
-        System.out.println("* Total no of student scored more than 40 marks in all modules - " + passStudents);
     }
 
+    /**
+     * Generates a detailed report of all students, sorted by their average marks in descending order.
+     * The report includes the student's ID, name, marks for each module, total marks, average marks, and grade.
+     * The grade is determined based on the student's average marks.
+     */
     private static void generateReport(){
-
+        if (studentCount > 0) {
+            System.out.println("\nReport of students\n");
+            Student[] avgSorted = new Student[studentCount];
+            System.arraycopy(students, 0, avgSorted, 0, studentCount);
+            for (int i = 0; i < avgSorted.length ; i++) {
+                for (int j = i + 1; j < avgSorted.length; j++) {
+                    if (avgSorted[i].averageMarks() < avgSorted[j].averageMarks()) {
+                        Student temp = avgSorted[i];
+                        avgSorted[i] = avgSorted[j];
+                        avgSorted[j] = temp;
+                    }
+                }
+            }
+            System.out.printf("%s | %-9s | %-25s | %s | %s | %s | %s | %-7s | %-11s | %n","Place", "Student ID", "Student Name", "Module 1 Marks", "Module 2 Marks", "Module 3 Marks", "Total Marks", "Average", "Grade");
+            System.out.println();
+            for (int i = 0; i < studentCount; i++) {
+                System.out.print(i+1);
+                avgSorted[i].report();
+            }
+            System.out.printf("%n %82s %n","-----End of the report-----");
+        }else{
+            System.err.println("There is no student details available to generate a report.");
+        }
     }
 }
--- src/Task03/Student.java
+++ src/Task03/Student.java
@@ -1,3 +1,11 @@
+/**
+ * Project Name: cw_StudentActionManagementSystem
+ * File Name: Student.java
+ * Description: This has the Student class related methods for the program.
+ * Author: Punjitha Bandara - w2083155
+ * Start Date: July 05, 2024
+ **/
+
 package Task03;
 
 public class Student {
@@ -5,27 +13,49 @@ public class Student {
     private final String ID;
     private Module[] module;
 
+    /**
+     * Constructs a new Student with the specified ID and name.
+     * Initializes the module array with default values.
+     * @param ID The unique identifier for the student.
+     * @param name The name of the student.
+     */
     public Student(String ID, String name){
         this.name = name;
         this.ID = ID;
         this.module = new Module[3];
-        module[0] = new Module(0.0);
-        module[1] = new Module(0.0);
-        module[2] = new Module(0.0);
+        module[0] = new Module(00.0);
+        module[1] = new Module(00.0);
+        module[2] = new Module(00.0);
     }
 
+    /**
+     * Returns the student's name.
+     * @return The name of the student.
+     */
     public String getName(){
         return this.name;
     }
 
+    /**
+     * Returns the student's ID.
+     * @return The unique ID of the student.
+     */
     public String getID(){
         return this.ID;
     }
 
+    /**
+     * Sets the student's modules with the given array of Module objects.
+     * @param module An array of Module objects representing the student's marks in different modules.
+     */
     public void setModule(Module[] module){
         this.module = module;
     }
 
+    /**
+     * Generates a comma-separated string of the student's marks in each module.
+     * @return A string representation of the student's marks in each module, separated by commas.
+     */
     public String studentModuleMark(){
         String[] studentMarks = new String[3];
         for (int i = 0; i < 3; i++) {
@@ -34,10 +64,19 @@ public class Student {
         return studentMarks[0] + studentMarks[1] + studentMarks[2];
     }
 
+    /**
+     * Sets the student's name.
+     * @param name The name of the student.
+     */
     public void setName(String name){
         this.name = name;
     }
 
+    /**
+     * Determines if the student has passed all modules.
+     * A student is considered to have passed a module if their marks are greater than 40.
+     * @return 1 if the student has passed all modules, 0 otherwise.
+     */
     public int countModulePassStudent(){
         if (module[0].getMarks() > 40 && this.module[1].getMarks() > 40 && this.module[2].getMarks() > 40){
             return 1;
@@ -46,6 +85,11 @@ public class Student {
         }
     }
 
+    /**
+     * Calculates the total marks obtained by the student.
+     * This method sums the marks obtained in each module to find the total score.
+     * @return The total marks obtained by the student.
+     */
     public Double totalMarks(){
         Double totalMarks = 0.0;
         for(Module marks : this.module){
@@ -54,10 +98,20 @@ public class Student {
         return totalMarks;
     }
 
+    /**
+     * Calculates the average marks obtained by the student.
+     * This method divides the total marks by the number of modules to find the average score.
+     * @return The average marks obtained by the student.
+     */
     public Double averageMarks(){
         return totalMarks() / 3;
     }
 
+    /**
+     * Determines the grade of the student based on their average marks.
+     * Grades are assigned as follows: Distinction (80+), Merit (70-79), Pass (40-69), and Fail (<40).
+     * @return The grade of the student as a string.
+     */
     public String grade(){
         Double averageMarks = averageMarks();
 
@@ -72,6 +126,15 @@ public class Student {
         }else {
             return "Fail";
         }
+    }
+
+    /**
+     * Prints a detailed report of the student's performance.
+     * The report includes the student's ID, name, marks for each module, total marks, average marks, and grade.
+     * This method formats the information into a readable string suitable for console output or file storage.
+     */
+    public void report(){
+        System.out.printf("%-4s | %-10s | %-25s | %05.2f  %-7s | %05.2f  %-7s | %05.2f  %-7s | %06.2f  %-3s | %06.2f %s | %-11s | %n","", this.ID,this.name,this.module[0].getMarks(),"",this.module[1].getMarks(),"",this.module[2].getMarks(),"",totalMarks(),"",averageMarks(),"",grade());
     }
 
 }
